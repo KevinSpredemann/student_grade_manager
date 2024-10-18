d092ad897bd3a91e8b3763e3af6afc72d6648c06
Kevin Ruan Spredemann <kevinspredemann50@gmail.com>
2024-10-17 21:59:33 -0300

    PUT and DELETE inserted

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ const server = http.createServer((req, res) => {
     });
 
     req.on('end', () => {
+        const id = url.split('/')[2];
+
         if (url === '/grades' && method === 'GET') {
             res.writeHead(200, { 'Content-Type': 'application/json' });
             res.end(JSON.stringify(grades));
@@ -23,7 +25,31 @@ const server = http.createServer((req, res) => {
             grades.push(newGrade);
             res.writeHead(201, { 'Content-Type': 'application/json' });
             res.end(JSON.stringify(newGrade));
-        } else {
+        } else if (url.startsWith('/grades/') && method === 'PUT') {
+            const { studentName, subject, grade } = JSON.parse(body);
+            const gradeToUpdate = grades.find(g => g.id === id);
+            if (gradeToUpdate) {
+                gradeToUpdate.studentName = studentName;
+                gradeToUpdate.subject = subject;
+                gradeToUpdate.grade = grade;
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify(gradeToUpdate));
+            } else {
+                res.writeHead(404, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ error: 'Grade not found' }));
+            }
+        } else if (url.startsWith('/grades/') && method === 'DELETE') {
+            const index = grades.findIndex(g => g.id === id);
+            if (index !== -1) {
+                grades.splice(index, 1);
+                res.writeHead(204);
+                res.end();
+            } else {
+                res.writeHead(404, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ error: 'Grade not found' }));
+            }
+        }
+         else {
             res.writeHead(404, { 'Content-Type': 'application/json' });
             res.end(JSON.stringify({ error: 'Route not found' }));
         }
